a63fb1e170e717d2e4abf9b9f3725b74cbff9f4e
Maximus Aurellus Bicalan <mjbicalan@up.edu.ph>
2025-06-02 15:37:15 +0800

    feat: implement UserSeeder to generate test users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,18 +5,18 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Log;
 
-class DatabaseSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-        $this->call([
-            UserSeeder::class,
-            // Add other seeders here as needed
-        ]);
+        $users = User::factory(3)->make();
+
+        // check the details of each user created (dont save to database)
+        dd($users->toArray());
     }
 }
