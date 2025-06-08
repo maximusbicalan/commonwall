4a43070b02736e00dabf3d493a3728c1604e07c4
Maximus Aurellus Bicalan <mjbicalan@up.edu.ph>
2025-06-08 18:41:10 +0800

    fix: correct table name from 'spin' to 'spins' in migration and add personal_access_tokens migration

--- database/migrations/2025_06_03_035908_create_spin_table.php
+++ database/migrations/2025_06_03_035908_create_spin_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('spin', function (Blueprint $table) {
+        Schema::create('spins', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->timestamps();
             $table->foreignUuid('user_id')->constrained('users')->onDelete('cascade');
